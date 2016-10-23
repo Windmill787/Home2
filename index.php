8a79ae7b47449b4e5a85f7cc2996b487bf54badf
Max Stoliarov <Windmill_78@mail.ru>
2016-10-23 19:33:58 +0300

    Test commit added Slim
    Added Slim script

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
+
 require_once 'vendor/autoload.php';
 
+$app = new Slim\App();
 
+$app->get('/', function ($request, $response, $args) {
+    $response->write("Welcome to Slim!");
+    return $response;
+});
 
+$app->run();
 
-
-
-?>
