638a161d6d056792e8220d0610f59292c74fdd50
Max Stoliarov <Windmill_78@mail.ru>
2016-10-23 18:23:08 +0300

    Test commit checking php and extensions
    Added check of php version and installed extensions

--- index.php
+++ index.php
@@ -1,18 +1,8 @@
 <?php
 require_once 'vendor/autoload.php';
 
-Twig_Autoloader::register();
-
-$loader = new Twig_Loader_Filesystem('templates');
-$twig = new Twig_Environment($loader);
 
 
-$template = $twig->loadTemplate('index.html');
-$title = "Title of page";
-echo $template->render(array(
-'title' => $title
-
-));
 
 
 
